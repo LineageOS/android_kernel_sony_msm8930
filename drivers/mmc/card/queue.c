bd1dfe359187d90fbb72fdc4a503ad0f0913c546
Avinaba Dalal <d97.avinaba@gmail.com>
2017-08-19 18:12:21 +0000

    Revert "mmc: Undo mmc changes that diverge us from upstream"
    This reverts commit 75a1793a3a1bff41eccca7422041580780991f4b.
    
    Change-Id: Icbfe15653774432f6579c4e33c9f54d22b884e3e

--- drivers/mmc/card/queue.c
+++ drivers/mmc/card/queue.c
@@ -57,6 +57,7 @@ static int mmc_queue_thread(void *d)
 {
 	struct mmc_queue *mq = d;
 	struct request_queue *q = mq->queue;
+	struct request *req;
 	struct mmc_card *card = mq->card;
 
 	current->flags |= PF_MEMALLOC;
@@ -64,7 +65,7 @@ static int mmc_queue_thread(void *d)
 	down(&mq->thread_sem);
 	do {
 		struct mmc_queue_req *tmp;
-		struct request *req = NULL;
+		req = NULL;	/* Must be set to NULL at each iteration */
 
 		spin_lock_irq(q->queue_lock);
 		set_current_state(TASK_INTERRUPTIBLE);
